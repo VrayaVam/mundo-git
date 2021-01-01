bf99248cbda4a0ca6222191587de6f25ae91daf4
Lucye <lethiciass.r@gmail.com>
2021-01-01 12:26:58 -0600

    Prueba test para Git y Github

---
+++ src/main/java/com/vraya/Saludo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.vraya;
+
+/**
+ * @author Xideral01
+ *
+ */
+public class Saludo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo Git y Github");
+
+	}
+
+}
